f3ee92cda38f2489a28c179949ab10c57e6361f5
Geoff B <38046772+geofferb@users.noreply.github.com>
2023-01-16 18:56:41 -0700

    made echoserver multithreaded

--- lab2/echo_server.py
+++ lab2/echo_server.py
@@ -1,4 +1,5 @@
 import socket
+from threading import Thread
 
 BYTES_TO_READ = 4096
 HOST = "127.0.0.1"
@@ -7,7 +8,7 @@ PORT = 8080
 # connect using echo "foobar" | nc localhost 8080 -q 1
 
 
-def handle_connecion(conn, addr):
+def handle_connection(conn, addr):
     with conn:
         print(f"Connected by {addr}")
         while True:
@@ -27,8 +28,21 @@ def start_server():
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.listen()
         conn, addr = s.accept()
-        handle_connecion(conn, addr)
+        handle_connection(conn, addr)
     return
 
 
-start_server()
+def start_thread_server():
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind((HOST, PORT))
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.listen(2)
+        while True:
+            conn, addr = server_socket.accept()
+            thread = Thread(target=handle_connection, args=(conn, addr))
+            thread.run()
+
+
+# start_server()
+start_thread_server()
